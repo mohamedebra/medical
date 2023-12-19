a137ab7efd2d5cb55aee59b8141a40061263abcc
mohamed <mohamedebra>
2023-12-20 00:59:23 +0200

    added images & svgs folder asstes added flutter_svg & flutter_screenutil maid_a_file_for_docapp_client added_navgation added folder routing that contains router and routes file added color and textstyles added empty login screen added onboarding screen

---
+++ lib/features/onboarding/wedgits/get_stared_bottom.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GetStaredBottom extends StatelessWidget {
+  const GetStaredBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: (){},
+        child: Text('')
+    );
+  }
+}
