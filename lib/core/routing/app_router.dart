d4b5607e1be0bfcd50f6d5a848fc2fd634de6906
mohamed <mohamedebra>
2023-12-24 11:16:03 +0200

    added app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medical/core/DI/dependency_injection.dart';
 import 'package:medical/core/routing/routes.dart';
+import 'package:medical/features/login/logic/login_cubit.dart';
 import 'package:medical/features/login/ui/login_screen.dart';
 import 'package:medical/features/onboarding/onboarding_screen.dart';
+
+import '../../features/home/ui/home_Screen.dart';
 
 class AppRouter{
 
@@ -15,7 +20,13 @@ class AppRouter{
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => LoginScreen()
+          builder: (_) => BlocProvider(
+              create: (context) => getIt<LoginCubit>(),
+              child: const LoginScreen())
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
         );
       default: return MaterialPageRoute(
           builder: (_) => Scaffold(
